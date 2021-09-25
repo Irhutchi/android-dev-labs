89bf7fe222f288be6b274cc9bb06d44337a6ba1e
Ian Hutchinson <ianhutch90@gmail.com>
2021-09-25 12:44:22 +0100

    Add cancel button to toolbar
    Incorporate a new 'Cancel' action into PlacemarkActivity. This will
    return to PlacemarkListActivity without adding a new Placemark.

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,25 +2,35 @@ package org.wit.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import org.wit.placemark.R
 import org.wit.placemark.databinding.ActivityPlacemarkBinding
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
-import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //setting up toolbar title (Add/Cancel)
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd) //(toolbarAdd is ID of toolbar)
+
         app = application as MainApp
+
         i("Placemark Activity started...")
+
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.description.text.toString()
@@ -33,12 +43,27 @@ class PlacemarkActivity : AppCompatActivity() {
                 //set result code and finish
                 setResult(RESULT_OK)
                 finish()
-            }
-            else {
-                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+            } else {
+                Snackbar.make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+
 
