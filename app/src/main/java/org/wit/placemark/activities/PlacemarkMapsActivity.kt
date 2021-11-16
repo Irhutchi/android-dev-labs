f924e674373924fe1819ff94ae82f7534de6b039
Ian <ianhutch90@gmail.com>
2021-11-16 22:18:16 +0000

    Add map marker and update placemark map activitiy

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,21 +1,76 @@
 package org.wit.placemark.activities
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
+import org.wit.placemark.R.*
+import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
+import org.wit.placemark.databinding.ContentPlacemarkMapsBinding
+import org.wit.placemark.main.MainApp
+
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPlacemarkMapsBinding
+    private lateinit var contentBinding: ContentPlacemarkMapsBinding
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placemark_maps)
-//        setSupportActionBar(toolbar)
+        app = application as MainApp
 
-//        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
+        binding = ActivityPlacemarkMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        contentBinding = ContentPlacemarkMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+        contentBinding.mapView.getMapAsync{
+            map = it
+            configureMap()
+        }
     }
 
+    fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.placemarks.findAll().forEach{
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
 }
